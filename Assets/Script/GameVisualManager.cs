e1dde621a5582d5225a6eac73e147a4595da4185
Agnesh K K <agneshk225@gmail.com>
2025-12-12 22:09:21 +0530

    package transfer completed

--- Assets/Script/GameVisualManager.cs
+++ Assets/Script/GameVisualManager.cs
@@ -2,7 +2,7 @@ using System.Data;
 using UnityEngine;
 using Unity.Netcode;
 
-public class GameVisualManager : MonoBehaviour
+public class GameVisualManager : NetworkBehaviour
 {
     private const float GRID_SIZE = 3.2f;
     [SerializeField] private Transform crossprefab;
@@ -14,10 +14,18 @@ public class GameVisualManager : MonoBehaviour
     }
     private void GameManager_OnClickedOnGridPosition(object sender,GameManager.OnClickedOnGridPositionEventArgs e)
     {
+        Debug.Log("message sended to server ");
+        SpawnedObjjectRpc(e.x , e.y);
+    } 
+    [Rpc(SendTo.Server)]
+    private void SpawnedObjjectRpc(int x ,int y)
+    {
+        Debug.Log("'Object Spawned");
         Transform SpawnedCrossTransform = Instantiate(crossprefab);
         SpawnedCrossTransform.GetComponent<NetworkObject>().Spawn(true);
-        SpawnedCrossTransform.position = GetGridWorldPosition(e.x,e.y);
+        SpawnedCrossTransform.position = GetGridWorldPosition(x,y);
     }
+    
     private Vector2 GetGridWorldPosition(int x ,int y)
     {
         return new Vector2(-GRID_SIZE + x * GRID_SIZE,-GRID_SIZE + y * GRID_SIZE);
